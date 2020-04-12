de84a6d31ae9d127f7aa1d7e214374b0cee05d52
obednarc <Mazury12*>
2020-04-12 21:54:07 +0200

    Zgłoszenie tematu

---
+++ src/main/java/asystent/insulinoopornosc/InsulinoopornoscApplication.java
@@ -0,0 +1,13 @@
+package asystent.insulinoopornosc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsulinoopornoscApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsulinoopornoscApplication.class, args);
+    }
+
+}
---
+++ src/test/java/asystent/insulinoopornosc/InsulinoopornoscApplicationTests.java
@@ -0,0 +1,13 @@
+package asystent.insulinoopornosc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsulinoopornoscApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
