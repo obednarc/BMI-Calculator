9a6aaadc02b0aadc76b75da15da6ca707e2c77c3
obednarc <Mazury12*>
2020-06-21 18:01:10 +0200

    JPA na Crud

--- src/main/java/panel/health/dao/UserRepository.java
+++ src/main/java/panel/health/dao/UserRepository.java
@@ -1,11 +1,12 @@
 package panel.health.dao;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import panel.health.model.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends CrudRepository<User, String> {
 
 }
